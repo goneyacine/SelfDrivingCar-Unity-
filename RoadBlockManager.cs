e1a9f4bf37119008ce527da8f876fd3dfd8eaef6
Yacine Betouche <goneyacine@gmail.com>
2023-10-01 15:12:46 +0100

    adding random road generation

--- RoadBlockManager.cs
+++ RoadBlockManager.cs
@@ -7,20 +7,31 @@ public class RoadBlockManager : MonoBehaviour
     bool obsticalsSpawned = false;
     public Transform nextRoadSpawnPoint;
     public GameObject roadBlockPrefab;
-    public 
-    void Start()
-    {
-        
-    }
+    public List<Transform> spawnPointsParents;
 
-    // Update is called once per frame
+    public GameObject obsticalPrefab;
+
+
     void Update()
     {
-        if(!obsticalsSpawned)
+        //randomly spawning obsticals
+        if (!obsticalsSpawned)
         {
-            //spawn obsticals here
+            foreach (Transform pointsParent in spawnPointsParents)
+            {
+               Transform randomPoint = pointsParent.transform.GetChild((int)Random.Range(0,pointsParent.transform.childCount - 1));
+               Instantiate(obsticalPrefab, randomPoint.position, Quaternion.identity);
+            }
             obsticalsSpawned = true;
         }
 
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(roadBlockPrefab, nextRoadSpawnPoint.position, Quaternion.identity);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       Destroy(gameObject);
     }
 }
